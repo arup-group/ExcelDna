4a504aab9be3fcbf4cb47de100c8db676fb68ac6
George.Aggrey <george.aggrey@arup.com>
2023-06-30 12:42:55 +0100

    Derive build version

--- MasterBuild/MasterBuild.cmd
+++ MasterBuild/MasterBuild.cmd
@@ -1,7 +1,7 @@
 setlocal
 
-set PackageVersion=1.7.0-ac1
-set PackageReferenceVersion=1.7.0-ac1
+set PackageVersion=%PACKAGE_VERSION%
+set PackageReferenceVersion=%PACKAGE_VERSION%
 set DllVersion=1.7.0.3
 
 set MSBuildPath="%MSBUILD_PATH%"
